0e44f140eba94c6855a0a2e52a9c9911ac241a96
timetam <timetam24@naver.com>
2023-10-06 15:45:03 +0900

    회원가입 페이지 초기세팅

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import LoginPage from "./Components/LoginPage";
+import JoinPage from "./Components/JoinPage";
 
 function App() {
   return (
     <div>
-      <LoginPage />
+      <JoinPage />
     </div>
   );
 }
